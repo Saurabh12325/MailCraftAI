53f89a9fa2e1a7437c4f2cd10f4f8777ae959402
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-07 20:04:48 +0530

    changes

--- src/main/java/com/mail/mailCraft/Controller/EmailController.java
+++ src/main/java/com/mail/mailCraft/Controller/EmailController.java
@@ -1,5 +1,16 @@
 package com.mail.mailCraft.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
 public class EmailController {
 
+    @PostMapping("/send")
+    public ResponseEntity<String> getEmailContent(){
+
+    }
 }
