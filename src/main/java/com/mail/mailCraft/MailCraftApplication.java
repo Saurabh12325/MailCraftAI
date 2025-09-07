615f84354d72fdc3d2a9198227a5963182a84e22
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-07 13:29:54 +0530

    changes

---
+++ src/main/java/com/mail/mailCraft/MailCraftApplication.java
@@ -0,0 +1,13 @@
+package com.mail.mailCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailCraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mail/mailCraft/MailCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.mailCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
